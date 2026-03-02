74bb68f5e13bccfaca7c04cc7144578b9e16d9c1
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2026-03-02 09:42:09 +0900

    feat: 投稿の詳細取得APIを作成

---
+++ app/api/blog/[id]/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import prisma from "../../../lib/prisma";
+import { main } from "../route";
+
+
+// GETリクエストで特定のブログ投稿を取得
+export const GET = async (req: Request) => {
+  try {
+    const id: number = parseInt(req.url.split("/blog/")[1]);
+    await main();
+    const post = await prisma.post.findFirst({ where: { id } });
+    return NextResponse.json({ message: "Success", post }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error", error: String(error) },
+      { status: 500 },
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
+};
--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -1,9 +1,5 @@
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../generated/prisma/client";
 import { NextResponse } from "next/server";
-
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-const prisma = new PrismaClient({ adapter });
+import prisma from "../../lib/prisma";
 
 export async function main() {
   try {
